642a4e09a0d1041504756e1218617e224c695b14
Crocho de ICT <crochodeict@gmail.com>
2018-04-02 14:31:33 +0700

    add picker, floating label, and submit button

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Alert } from 'react-native';
 import {
-  Container, Header,
+  Button, Container, Header,
   Content, Form, Item,
   Input, Label, ListItem,
-  Text, Right, Radio
+  Text, Right, Radio,
+  Picker
 } from 'native-base';
 
 export default class App extends Component<Props> {
@@ -15,7 +16,8 @@ export default class App extends Component<Props> {
 
     this.state = {
       income: true,
-      expense: false
+      expense: false,
+      category: undefined
     };
 
   }
@@ -23,30 +25,60 @@ export default class App extends Component<Props> {
   changeIncome = () => this.setState({ income: true, expense: false });
   changeExpense = () => this.setState({ income: false, expense: true });
 
+  onCategoryChange(value: string) {
+    this.setState({
+      category: value
+    });
+  }
+
   render() {
     return (
       <Container>
         <Header />
-        <Content>
+        <Content padder>
           <Form>
+
             <ListItem>
               <Text style={{flex: 1}}>Income</Text>
               <Right style={{flex: 0}}>
                 <Radio onPress={this.changeIncome} selected={this.state.income} />
               </Right>
             </ListItem>
+
             <ListItem>
               <Text style={{flex: 1}}>Expense</Text>
               <Right style={{flex: 0}}>
                 <Radio onPress={this.changeExpense} selected={this.state.expense} />
               </Right>
             </ListItem>
-            <Item regular>
-              <Input placeholder='Amount' />
+
+            <Picker
+              mode="dropdown"
+              placeholder="Choose a Category"
+              selectedValue={this.state.category}
+              onValueChange={this.onCategoryChange.bind(this)}
+              style={{marginLeft: 8, marginRight: 14}}
+            >
+              <Item label="Food" value="food" />
+              <Item label="Drink" value="drink" />
+              <Item label="Health" value="health" />
+              <Item label="Travel" value="travel" />
+            </Picker>
+
+            <Item floatingLabel>
+              <Label>Amount</Label>
+              <Input />
             </Item>
-            <Item regular>
-              <Input placeholder='Description' />
+
+            <Item floatingLabel>
+              <Label>Note</Label>
+              <Input />
             </Item>
+
+            <Button block style={{marginTop: 50}}>
+              <Text>Submit</Text>
+            </Button>
+
           </Form>
         </Content>
       </Container>
